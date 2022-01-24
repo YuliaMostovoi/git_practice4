80f3f22c5edcb02eb75aa9972d5b68c5ca0d31b4
YuliaMostovoi <yulia.mostovoi@gmail.com>
2022-01-24 10:38:10 -0500

    Yulia Mostovoi

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("\"Hello World\" = " + "Hello World");
+
+        System.out.println("\"Yulia Mostovoi\" = " + "Yulia Mostovoi");
     }
 
 
